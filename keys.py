5a2a846ff95e88b09f4ebf64e639e84e01e8286d
Jonas <jonasmoreira076@gmail.com>
2023-02-02 12:39:56 -0400

    alteração de token

--- keys.py
+++ keys.py
@@ -1,3 +1,3 @@
 # SID e token de autenticação disponibilizado pelo Twilio
 account_sid = 'ACf8788a37fa18df6de4eeec2fa3afe32f'
-auth_token =  'dbe9bfc8df16976c736459cc11a6d622'
+auth_token =  'c0c48631784433ee155bdfe581f4a16e'
