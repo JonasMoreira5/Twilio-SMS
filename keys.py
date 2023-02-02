9052b6dfd2ba0b8873bcee672e7200f54b280b74
Jonas <jonasmoreira076@gmail.com>
2023-02-02 12:36:26 -0400

    parte 1

---
+++ keys.py
@@ -0,0 +1,3 @@
+# SID e token de autenticação disponibilizado pelo Twilio
+account_sid = 'ACf8788a37fa18df6de4eeec2fa3afe32f'
+auth_token =  'dbe9bfc8df16976c736459cc11a6d622'
---
+++ main.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+import keys
+
+client = Client(keys.account_sid, keys.auth_token)
+
+message = client.messages \
+                .create(
+                     body="Olá meu nome é Jonas Moreira Barbosa e estou participando do PROGRAMA DE ESTÁGIO TALENT LAB ITACOATIARA.",
+                     from_='+12052739236',
+                     to='+5592994041016'
+                 )
+print("Mensagem Envida!")
+print(message.sid)
